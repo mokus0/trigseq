36ccefb66303441a6a9703c8be710bf6558d0409
James Cook <mokus@deepbondi.net>
2010-07-08 01:31:55 +0000

    Comment

--- src/Math/Sequence/Trig.hs
+++ src/Math/Sequence/Trig.hs
@@ -1,9 +1,23 @@
 {-# LANGUAGE BangPatterns #-}
 module Math.Sequence.Trig where
 
+-- TODO: clean up this comment, I got a bit long-winded.
+
 -- |Efficient recurrence for cosine and sine sequences with regularly-spaced
 -- parameters.  @trigSeq theta delta@ === @zip (map sin thetas) (map cos thetas)@
 -- where @thetas = iterate (+delta) theta@.
+-- 
+-- This recurrence is stable, but not always appropriate.  In typical situations,
+-- error is dominated by drift in the original enumeration (for example,
+-- in @[0,1/3 ..]@, 1/3 is inexact and the drift from this minor error will 
+-- propagate differently in the enumeration than in this recurrence, leading 
+-- to drift when comparing the expressions above for long sequences).  If the
+-- original enumeration is exact (eg, @[0,0.125..]@) then the drift is quite 
+-- slow, with a typical maximum error on the order of 1e-13 over a list of 
+-- 100000 'Double's (compared to 1e-10 in the case where the enumeration is 
+-- computed at higher precision, or 1e-7 when the enumeration is computed naively
+-- - note that in the latter case the error is in the enumeration, not the
+-- recurrence).
 trigSeq :: Floating a => a -> a -> [(a,a)]
 trigSeq theta delta = go (sin theta) (cos theta)
     where
